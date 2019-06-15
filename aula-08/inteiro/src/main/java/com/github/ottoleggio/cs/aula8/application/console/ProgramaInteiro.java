ecfcc0a463ef41d2a27498488631354e89021b9c
Octavio Lo Leggio <ottoleggio@gmail.com>
2019-06-15 16:48:25 -0300

    correcoes pmd

--- aula-08/inteiro/src/main/java/com/github/ottoleggio/cs/aula8/application/console/ProgramaInteiro.java
+++ aula-08/inteiro/src/main/java/com/github/ottoleggio/cs/aula8/application/console/ProgramaInteiro.java
@@ -1,15 +1,33 @@
 package com.github.ottoleggio.cs.aula8.application.console;
 
 import java.io.IOException;
-import com.github.ottoleggio.cs.aula8.domain.ImprimeHexa;
+import java.util.logging.Logger;
 
+import com.github.ottoleggio.cs.aula8.domain.ImprimeHexaUtils;
+
+/**
+* Executa ProgramaInteiro.
+*
+* @author Octavio Lo Leggio
+*/
 public final class ProgramaInteiro {
-
+    /**
+    * Mensagem de retorno do programa.
+    */
+    private static Logger log = Logger.getLogger(ProgramaInteiro.class.getName());
+    /**
+    * Método construtor da classe main.
+    */
     private ProgramaInteiro() {
 
     }
-
+    /**
+    * Método main.
+    * @param args String de endereço de um arquivo
+    *
+    * @throws IOException se ocorrer exception de IO
+    */
     public static void main(final String[] args) throws IOException {
-        System.out.println(ImprimeHexa.leByteFile(args[0]));
+        log.fine(ImprimeHexaUtils.leByteFile(args[0]));
     }
 }
