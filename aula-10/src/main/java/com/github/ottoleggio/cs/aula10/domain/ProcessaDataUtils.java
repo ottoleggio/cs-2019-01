398663aa85a72620bb77e6ffbb67307c9b3e27f4
Octavio Lo Leggio <ottoleggio@gmail.com>
2019-06-20 13:52:55 -0300

    inclusao ultimoDiaDoMes e testes

--- aula-10/src/main/java/com/github/ottoleggio/cs/aula10/domain/ProcessaDataUtils.java
+++ aula-10/src/main/java/com/github/ottoleggio/cs/aula10/domain/ProcessaDataUtils.java
@@ -69,4 +69,43 @@ public final class ProcessaDataUtils {
         }
         return false;
     }
+
+    /**
+     * Identifica o último dia do mês de acordo com 
+     * a data fornecida e o ano bissexto calculado.
+     *
+     * @param mes mês em que se deseja saber o último dia
+     * @param ano ano para se saber se é bissexto ou não
+     * @param bissextoRef Ano bissexto de referência
+     *
+     * @return um int representando o último dia do mês
+     */
+    public static int UltimoDiaDoMes(final int mes, final int ano,
+            final int bissextoRef) {
+        boolean seBissexto = SeBissexto(ano, bissextoRef);
+
+        switch(mes) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            return 31;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            return 30;
+        case 2:
+            if(seBissexto) {
+                return 29;
+            } else {
+                return 28;
+            }
+        default:
+            return 0;
+        }
+    }
 }
