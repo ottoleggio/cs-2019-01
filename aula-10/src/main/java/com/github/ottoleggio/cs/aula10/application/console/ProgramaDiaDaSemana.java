e06ab74fbde881d44fe6d8231e78d5d6b753931c
Octavio Lo Leggio <ottoleggio@gmail.com>
2019-06-21 14:29:22 -0300

    implementacao executaPrograma

--- aula-10/src/main/java/com/github/ottoleggio/cs/aula10/application/console/ProgramaDiaDaSemana.java
+++ aula-10/src/main/java/com/github/ottoleggio/cs/aula10/application/console/ProgramaDiaDaSemana.java
@@ -24,18 +24,15 @@ public final class ProgramaDiaDaSemana {
     * @throws IOException se ocorrer exception de IO
     */
     public static void main(final String[] args) {
-/*
-        String[] entrada = new String[4];
-        entrada[0] = "20181232";
-        entrada[1] = "2000";
-        entrada[2] = "20181231";
-        entrada[3] = "2";
-        System.out.println(ValidaEntradaUtils.validadorEntrada(entrada));
-        System.exit(ValidaEntradaUtils.validadorEntrada(entrada));
 
-        System.out.println(ProcessaDataUtils.recuaData("20191231", "20170101", 2018));
-        System.out.println(ProcessaDataUtils.iteraData("20170101", "20191231", 2018));
-        */
-        System.out.println(ProcessaDataUtils.recuaDiaDaSemana(8, 0));
+        String[] entrada = new String[4];
+        entrada[0] = "20160312";
+        entrada[1] = "2016";
+        entrada[2] = "20160228";
+        entrada[3] = "5";
+        /*     
+        System.out.println(ProcessaDataUtils.executaPrograma(entrada[0], entrada[1], entrada[2], entrada[3]));
+*/
+        System.exit(ProcessaDataUtils.executaPrograma(entrada[0], entrada[1], entrada[2], entrada[3]));
     }
 }
